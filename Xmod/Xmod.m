a191e62e6c5760e7de4cb925fcd2bb2545fea63e
rentzsch <jwr.git@redshed.net>
2007-09-06 05:32:01 +0000

    [NEW] Xmo'd for Xcode 2.x.

--- Xmod/Xmod.m
+++ Xmod/Xmod.m
@@ -1,27 +1,25 @@
 #import "Xmod.h"
 #import "MethodSwizzle.h"
 
-@interface NSObject (mogenerator_saveModelToFile)
-@end
-@implementation NSObject (mogenerator_saveModelToFile)
+NSBundle *selfBundle;
 
-- (BOOL)mogenerator_saveModelToFile:(NSString*)modelPackagePath_ {
-	BOOL result = [self mogenerator_saveModelToFile:modelPackagePath_];
+@interface NSObject (xmod_saveModelToFile)
+@end
+@implementation NSObject (xmod_saveModelToFile)
+
+- (BOOL)xmod_saveModelToFile:(NSString*)modelPackagePath_ {
+	BOOL result = [self xmod_saveModelToFile:modelPackagePath_];
 	if (result) {
-		NSString *modelPackageFolderPath = [modelPackagePath_ stringByDeletingLastPathComponent];
-		NSString *mosFolderPath = [modelPackageFolderPath stringByAppendingPathComponent:@"MOs"];
-		
-		BOOL isDirectory;
-		if ([[NSFileManager defaultManager] fileExistsAtPath:mosFolderPath isDirectory:&isDirectory] && isDirectory) {
-			NSTask *mogenerator = [[[NSTask alloc] init] autorelease];
-			[mogenerator setLaunchPath:@"/usr/bin/mogenerator"];
-			[mogenerator setCurrentDirectoryPath:mosFolderPath];
-			// -model ../MyDocument.xcdatamodel -includem include.mm
-			[mogenerator setArguments:[NSArray arrayWithObjects:
-				@"-model", modelPackagePath_,
-				@"-includem", @"include.m",
-				nil]];
-			[mogenerator launch];
+		NSString *scriptPath = [selfBundle pathForResource:@"Xmod" ofType:@"scpt" inDirectory:@"Scripts"];
+		NSAssert(scriptPath, @"failed to find Xmod.scpt");
+		NSDictionary *scriptInitError = nil;
+		NSAppleScript *script = [[[NSAppleScript alloc] initWithContentsOfURL:[NSURL fileURLWithPath:scriptPath]
+																		error:&scriptInitError] autorelease];
+		NSAssert1(!scriptInitError, @"failed to init Xmod.scpt: %@", scriptInitError);
+		if (!scriptInitError) {
+			NSDictionary *scriptExecuteError = nil;
+			[script executeAndReturnError:&scriptExecuteError];
+			NSAssert1(!scriptInitError, @"failed to execute Xmod.scpt: %@", scriptExecuteError);
 		}
 	}
 	return result;
@@ -32,6 +30,8 @@
 @implementation Xmod
 
 + (void)pluginDidLoad:(NSBundle*)bundle_ {
+	selfBundle = bundle_;
+	
 	//	Force loading of the Core Data XDesign plugin so we can find the class to swizzle its instance method.
 	NSBundle *coreDataPlugin = [NSBundle bundleWithPath:@"/Library/Application Support/Apple/Developer Tools/Plug-ins/XDCoreDataModel.xdplugin"];
 	NSAssert(coreDataPlugin, @"failed to load XDCoreDataModel.xdplugin");
@@ -42,7 +42,7 @@
 	
 	BOOL swizzled = MethodSwizzle(persistenceDocumentController,
 								  @selector(saveModelToFile:),
-								  @selector(mogenerator_saveModelToFile:));
+								  @selector(xmod_saveModelToFile:));
 	NSAssert(swizzled, @"failed to swizzle -[XDPersistenceDocumentController saveModelToFile:]");
 }
 
