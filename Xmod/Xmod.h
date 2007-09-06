6473be97c45b7e51ddeba8149ebef0d9e9315123
rentzsch <jwr.git@redshed.net>
2007-09-06 08:30:36 +0000

    [NEW] Xmo'd: Autocustomize Entity Classes Xcode 2.x menu item.

--- Xmod/Xmod.h
+++ Xmod/Xmod.h
@@ -1,4 +1,6 @@
 #import <Cocoa/Cocoa.h>
+#import <objc/objc-class.h>
 
 @interface Xmod : NSObject {}
+- (IBAction)autocustomizeEntityClasses:(id)sender_;
 @end
--- Xmod/Xmod.m
+++ Xmod/Xmod.m
@@ -3,34 +3,37 @@
 
 NSBundle *selfBundle;
 
+static void runScriptNamed(NSString *scriptName) {
+	NSString *scriptPath = [selfBundle pathForResource:scriptName ofType:@"scpt" inDirectory:@"Scripts"];
+	NSCAssert1(scriptPath, @"failed to find %@.scpt", scriptName);
+	NSDictionary *scriptInitError = nil;
+	NSAppleScript *script = [[[NSAppleScript alloc] initWithContentsOfURL:[NSURL fileURLWithPath:scriptPath]
+																	error:&scriptInitError] autorelease];
+	NSCAssert2(!scriptInitError, @"failed to init %@.scpt: %@", scriptName, scriptInitError);
+	if (!scriptInitError) {
+		NSDictionary *scriptExecuteError = nil;
+		[script executeAndReturnError:&scriptExecuteError];
+		NSCAssert2(!scriptInitError, @"failed to execute %@.scpt: %@", scriptName, scriptExecuteError);
+	}
+}
+
 @interface NSObject (xmod_saveModelToFile)
 @end
 @implementation NSObject (xmod_saveModelToFile)
-
 - (BOOL)xmod_saveModelToFile:(NSString*)modelPackagePath_ {
 	BOOL result = [self xmod_saveModelToFile:modelPackagePath_];
 	if (result) {
-		NSString *scriptPath = [selfBundle pathForResource:@"Xmod" ofType:@"scpt" inDirectory:@"Scripts"];
-		NSAssert(scriptPath, @"failed to find Xmod.scpt");
-		NSDictionary *scriptInitError = nil;
-		NSAppleScript *script = [[[NSAppleScript alloc] initWithContentsOfURL:[NSURL fileURLWithPath:scriptPath]
-																		error:&scriptInitError] autorelease];
-		NSAssert1(!scriptInitError, @"failed to init Xmod.scpt: %@", scriptInitError);
-		if (!scriptInitError) {
-			NSDictionary *scriptExecuteError = nil;
-			[script executeAndReturnError:&scriptExecuteError];
-			NSAssert1(!scriptInitError, @"failed to execute Xmod.scpt: %@", scriptExecuteError);
-		}
+		runScriptNamed(@"Xmod");
 	}
 	return result;
 }
-
 @end
 
 @implementation Xmod
 
 + (void)pluginDidLoad:(NSBundle*)bundle_ {
 	selfBundle = bundle_;
+	[[self alloc] init];
 	
 	//	Force loading of the Core Data XDesign plugin so we can find the class to swizzle its instance method.
 	NSBundle *coreDataPlugin = [NSBundle bundleWithPath:@"/Library/Application Support/Apple/Developer Tools/Plug-ins/XDCoreDataModel.xdplugin"];
@@ -44,6 +47,32 @@ NSBundle *selfBundle;
 								  @selector(saveModelToFile:),
 								  @selector(xmod_saveModelToFile:));
 	NSAssert(swizzled, @"failed to swizzle -[XDPersistenceDocumentController saveModelToFile:]");
+}
+
+- (id)init {
+	self = [super init];
+	if (self) {
+		[[NSNotificationCenter defaultCenter] addObserver:self
+												 selector:@selector(applicationDidFinishLaunching:)
+													 name:NSApplicationDidFinishLaunchingNotification
+												   object:nil];
+	}
+	return self;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification*)notification_ {
+	NSMenu *designMenu = [[[NSApp mainMenu] itemWithTitle:@"Design"] submenu];
+	NSMenu *dataModelMenu = [[designMenu itemWithTitle:@"Data Model"] submenu];
+	
+	NSMenuItem *myMenuItem = [dataModelMenu insertItemWithTitle:@"Autocustomize Entity Classes"
+														 action:@selector(autocustomizeEntityClasses:)
+												  keyEquivalent:@""
+														atIndex:0];
+	[myMenuItem setTarget:self];
+}
+
+- (IBAction)autocustomizeEntityClasses:(id)sender_ {
+	runScriptNamed(@"Autocustomize Entity Classes");
 }
 
 @end
