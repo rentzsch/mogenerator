ec86e5aa4eb5951feaefede6e860362df0843c98
rentzsch <jwr.git@redshed.net>
2008-08-05 15:02:28 +0000

    [NEW] mogenerator: Add preliminary fetch request wrapper support.

---
+++ mogenerator.h
@@ -0,0 +1,52 @@
+/*******************************************************************************
+	mogenerator.h
+		Copyright (c) 2006-2008 Jonathan 'Wolf' Rentzsch: <http://rentzsch.com>
+		Some rights reserved: <http://opensource.org/licenses/mit-license.php>
+
+	***************************************************************************/
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "MiscMergeTemplate.h"
+#import "MiscMergeCommandBlock.h"
+#import "MiscMergeEngine.h"
+#import "FoundationAdditions.h"
+#import "nsenumerate.h"
+#import "NSString+MiscAdditions.h"
+#import "DDCommandLineInterface.h"
+
+@interface NSEntityDescription (customBaseClass)
+- (BOOL)hasCustomSuperentity;
+- (NSString*)customSuperentity;
+- (void)_processPredicate:(NSPredicate*)predicate_ bindings:(NSMutableArray*)bindings_;
+- (NSArray*)prettyFetchRequests;
+@end
+
+@interface NSAttributeDescription (scalarAttributeType)
+- (BOOL)hasScalarAttributeType;
+- (NSString*)scalarAttributeType;
+- (BOOL)hasDefinedAttributeType;
+- (NSString*)objectAttributeType;
+@end
+
+@interface NSString (camelCaseString)
+- (NSString*)camelCaseString;
+@end
+
+@interface MOGeneratorApp : NSObject <DDCliApplicationDelegate> {
+	NSString				*tempMOMPath;
+	NSManagedObjectModel	*model;
+	NSString				*baseClass;
+	NSString				*includem;
+	NSString				*templatePath;
+	NSString				*outputDir;
+	NSString				*machineDir;
+	NSString				*humanDir;
+	NSString				*templateGroup;
+	BOOL					_help;
+	BOOL					_version;
+}
+
+- (NSString*)appSupportFileNamed:(NSString*)fileName_;
+@end
