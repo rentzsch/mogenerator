26584d28b7fd389d4b8ee3220ef26b0b8879ef83
rentzsch <jwr.git@redshed.net>
2008-10-14 10:21:42 +0000

    [NEW] mogenerator 1.13.1: fix typo introduced in r314 where I was using -(will|Did)AccessValueForKey: in relationship NSMutableSet getter when I should have been using -(will|Did)AChangeValueForKey:. (Pascal Augustin)

--- mogenerator.m
+++ mogenerator.m
@@ -332,7 +332,7 @@ NSString *ApplicationSupportSubdirectoryName = @"mogenerator";
     
     if (_version)
     {
-        printf("mogenerator 1.13. By Jonathan 'Wolf' Rentzsch + friends.\n");
+        printf("mogenerator 1.13.1. By Jonathan 'Wolf' Rentzsch + friends.\n");
         return EXIT_SUCCESS;
     }
     
