cc98db287d33156d5712c815870e6f61d9c1a2af
rentzsch <jwr.git@redshed.net>
2011-07-21 11:41:00 -0500

    Re-add JRSwizzle as a fake submodule.

---
+++ Xmod/JRSwizzle/JRSwizzle.h
@@ -0,0 +1,11 @@
+//	Copyright (c) 2007-2011 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
+//	Some rights reserved: http://opensource.org/licenses/mit-license.php
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (JRSwizzle)
+
++ (BOOL)jr_swizzleMethod:(SEL)origSel_ withMethod:(SEL)altSel_ error:(NSError**)error_;
++ (BOOL)jr_swizzleClassMethod:(SEL)origSel_ withClassMethod:(SEL)altSel_ error:(NSError**)error_;
+
+@end
