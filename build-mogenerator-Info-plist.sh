52433043d2b48fccf726fd4f8bcc48e9b5f91d4e
Andrew Frederick <andrew.frederick@mutualmobile.com>
2014-06-23 15:44:47 -0500

    Updated script to stop assertion on line mogenerator.m:529

--- build-mogenerator-Info-plist.sh
+++ build-mogenerator-Info-plist.sh
@@ -3,6 +3,8 @@ rm -f /tmp/mogenerator-Info.plist
 	-c "Clear" \
 	-c "Import :human.h.motemplate templates/human.h.motemplate" \
 	-c "Import :human.m.motemplate templates/human.m.motemplate" \
+	-c "Import :human.swift.motemplate templates/human.swift.motemplate" \
 	-c "Import :machine.h.motemplate templates/machine.h.motemplate" \
 	-c "Import :machine.m.motemplate templates/machine.m.motemplate" \
+	-c "Import :machine.swift.motemplate templates/machine.swift.motemplate" \
 	/tmp/mogenerator-Info.plist
