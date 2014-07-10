87daa8fcd09568f3b99288cbe713068ec303585e
rentzsch <jwr.git@redshed.net>
2014-07-10 13:35:22 -0500

    Initial Commit

---
+++ test/MogenSwiftTest/MogenSwiftTest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MogenSwiftTest
+//
+//  Created by Wolf Rentzsch on 7/10/14.
+//  Copyright (c) 2014 Jonathan 'Wolf' Rentzsch. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
